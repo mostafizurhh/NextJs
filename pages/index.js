1ef33a27546e82a57e5dad970fa91b309fa87fc7
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-12-30 22:25:37 +0000

    banner and footer added

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Banner from "../Components/Banner/Banner";
+import Footer from "../Components/Footer/Footer";
 
 export default function Home() {
   return (
-    <div >
+    <div className="text-center" >
       {/* Head component >> to provide dynamic page title */}
       <Head>
         <title>Home Page</title>
       </Head>
-      <h1 className="mb-5">Hello Next JS</h1>
+      <Banner></Banner>
+      <h1 className="my-5">Hello Next JS</h1>
       {/* link component >> to navigate from one page to another */}
       <Link href='/blog'><button className="btn btn-success btn-sm">Go to Blog</button></Link>
       <Link href='/AllPosts'><button className="btn btn-success btn-sm ml-5">Go to Posts</button></Link>
       <Link href='/Comments'><button className="btn btn-success btn-sm ml-5">Go to Comments</button></Link>
-      <div className="mt-10 ml-8">
+      <div className="flex justify-center items-center py-10">
         <Image src='/sm1.png' width={300} height={900} alt=''></Image>
       </div>
+      <Footer></Footer>
     </div>
   )
 }
