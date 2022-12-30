5373ab6d79a8cb27efe95fd584f442a0335bbf08
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-12-30 11:06:11 +0000

    added page title using built-in Head Component og Next.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const blog = () => {
     return (
         <div>
+            {/* Head component >> to provide dynamic page title */}
+            <Head>
+                <title>Blog Page</title>
+            </Head>
             <h1>this is blog page</h1>
             {/* link component >> to navigate from one page to another */}
             <Link href='/'>Home</Link>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div >
+      {/* Head component >> to provide dynamic page title */}
+      <Head>
+        <title>Home Page</title>
+      </Head>
       <h1>Hello Next JS</h1>
       {/* link component >> to navigate from one page to another */}
       <Link href='blog'>Go to Blog</Link>
